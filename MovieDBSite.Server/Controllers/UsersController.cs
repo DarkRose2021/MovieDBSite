b7564ba66bd0d1824072fae9e2d60b8c4d4850b2
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-01-11 21:14:31 -0700

    Started the API. Still learning this though

---
+++ MovieDBSite.Server/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieDBSite.Server.Models;
+
+namespace MovieDBSite.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		//For testing purposes only
+		private readonly User _sampleUser = new User { Username = "demo", Password = "password" };
+
+		[HttpPost("login")]
+		public ActionResult<string> Login([FromBody] User user)
+		{
+			if (user.Username == _sampleUser.Username && user.Password == _sampleUser.Password)
+			{
+				// Successful login
+				return Ok("Login successful");
+			}
+
+			// Unauthorized
+			return Unauthorized("Invalid credentials");
+		}
+	}
+}
